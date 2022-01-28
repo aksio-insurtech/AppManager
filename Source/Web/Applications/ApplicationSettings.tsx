dad26469910c0327019b7c146e0ca5338bb4b16a
Einar Ingebrigtsen <einari@me.com>
2022-01-28 11:24:58 +0100

    Adding danger zone

--- Source/Web/Applications/ApplicationSettings.tsx
+++ Source/Web/Applications/ApplicationSettings.tsx
@@ -1,7 +1,16 @@
 // Copyright (c) Aksio Insurtech. All rights reserved.
+
+import { PrimaryButton, Stack } from '@fluentui/react';
+
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 export const ApplicationSettings = () => {
     return (
-        <>Settings</>
+        <Stack>
+
+            <h2>Danger zone</h2>
+            <PrimaryButton text="Delete" />
+
+        </Stack>
+
     );
 };
