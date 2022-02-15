3e4db66bcc11445a48519ed7ffcfa95b07c3903c
Einar Ingebrigtsen <einari@me.com>
2022-02-15 19:36:19 +0100

    Adding a mechanism for cleaning up pending operations

--- Source/Reactions/Applications/PulumiRunner.cs
+++ Source/Reactions/Applications/PulumiRunner.cs
@@ -1,7 +1,11 @@
 // Copyright (c) Aksio Insurtech. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System.Text.Encodings.Web;
+using System.Text.Json;
+using System.Text.Json.Nodes;
 using Common;
+using Microsoft.Extensions.Logging;
 using Pulumi.Automation;
 
 namespace Reactions.Applications
@@ -11,9 +15,14 @@ namespace Reactions.Applications
     /// </summary>
     public class PulumiRunner : IPulumiRunner
     {
-        readonly IApplicationSettings _applicationSettings;
+        readonly ILogger<PulumiRunner> _logger;
+        readonly ISettings _settings;
 
-        public PulumiRunner(IApplicationSettings applicationSettings) => _applicationSettings = applicationSettings;
+        public PulumiRunner(ILogger<PulumiRunner> logger, ISettings applicationSettings)
+        {
+            _logger = logger;
+            _settings = applicationSettings;
+        }
 
         /// <inheritdoc/>
         public void Up(Application application, string name, PulumiFn definition, RuntimeEnvironment environment)
@@ -52,6 +61,7 @@ namespace Reactions.Applications
                 ProjectSettings = new(name, ProjectRuntimeName.Dotnet)
             };
             var stack = await LocalWorkspace.CreateOrSelectStackAsync(args);
+            await RemovePendingOperations(stack);
             await stack.Workspace.InstallPluginAsync("azure-native", "1.54.0");
             await stack.Workspace.InstallPluginAsync("mongodbatlas", "3.2.0");
             await stack.SetAllConfigAsync(new Dictionary<string, ConfigValue>
@@ -60,15 +70,36 @@ namespace Reactions.Applications
                 { "azure-native:subscriptionId", new ConfigValue(application.AzureSubscriptionId.ToString()) }
             });
 
-            var mongoDBPublicKey = _applicationSettings.MongoDBPublicKey;
-            var mongoDBPrivateKey = _applicationSettings.MongoDBPrivateKey;
+            var mongoDBPublicKey = _settings.MongoDBPublicKey;
+            var mongoDBPrivateKey = _settings.MongoDBPrivateKey;
             Environment.SetEnvironmentVariable("MONGODB_ATLAS_PUBLIC_KEY", mongoDBPublicKey);
             Environment.SetEnvironmentVariable("MONGODB_ATLAS_PRIVATE_KEY", mongoDBPrivateKey);
 
-            var accessToken = _applicationSettings.PulumiAccessToken;
+            var accessToken = _settings.PulumiAccessToken;
             Environment.SetEnvironmentVariable("PULUMI_ACCESS_TOKEN", accessToken.ToString());
 
             return stack;
+        }
+
+        async Task RemovePendingOperations(WorkspaceStack stack)
+        {
+            _logger.ExportStack();
+
+            var stackDeployment = await stack.ExportStackAsync();
+            var jsonNode = JsonNode.Parse(stackDeployment.Json.GetRawText())!.AsObject();
+            var deployment = jsonNode!["deployment"]!.AsObject();
+            deployment.Remove("pending_operations");
+            deployment.Add("pending_operations", new JsonArray());
+            var jsonString = jsonNode.ToJsonString(new JsonSerializerOptions()
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping
+            });
+            await File.WriteAllTextAsync("newState.json", jsonString);
+            stackDeployment = StackDeployment.FromJsonString(jsonString);
+
+            _logger.ImportStack();
+            await stack.ImportStackAsync(stackDeployment);
         }
     }
 }
