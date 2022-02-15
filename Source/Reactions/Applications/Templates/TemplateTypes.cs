8d0017c0019e83ecd1b3beee5ce9e3e4e43a7758
Einar Ingebrigtsen <einari@me.com>
2022-02-15 19:34:36 +0100

    Adding accessor class for accessing templates

---
+++ Source/Reactions/Applications/Templates/TemplateTypes.cs
@@ -0,0 +1,36 @@
+// Copyright (c) Aksio Insurtech. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Aksio.Cratis.Strings;
+using HandlebarsDotNet;
+
+namespace Reactions.Applications.Templates
+{
+    /// <summary>
+    /// Holds all the available templates.
+    /// </summary>
+    public static class TemplateTypes
+    {
+        /// <summary>
+        /// The template for 'appsettings.json'.
+        /// </summary>
+        public static readonly HandlebarsTemplate<object, object> AppSettings = Handlebars.Compile(GetTemplate("appsettings.json"));
+
+        static TemplateTypes()
+        {
+            Handlebars.RegisterHelper("camelcase", (writer, _, parameters) => writer.WriteSafeString(parameters[0].ToString()!.ToCamelCase()));
+        }
+
+        static string GetTemplate(string name)
+        {
+            var rootType = typeof(TemplateTypes);
+            var stream = rootType.Assembly.GetManifestResourceStream($"{rootType.Namespace}.{name}.hbs");
+            if (stream != default)
+            {
+                using var reader = new StreamReader(stream);
+                return reader.ReadToEnd();
+            }
+            return string.Empty;
+        }
+    }
+}
