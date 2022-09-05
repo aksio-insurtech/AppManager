2ec944b4a14e244c054fb3b0d4a41bef0cbf014d
Einar Ingebrigtsen <einari@me.com>
2022-09-05 20:48:53 +0200

    Removing commented out project list access

--- Source/Reactions/Applications/Pulumi/ApplicationMongoDBPulumiExtensions.cs
+++ Source/Reactions/Applications/Pulumi/ApplicationMongoDBPulumiExtensions.cs
@@ -115,11 +115,6 @@ public static class ApplicationMongoDBPulumiExtensions
             }
         });
 
-        // _ = new ProjectIpAccessList("kernel", new()
-        // {
-        //     ProjectId = project.Id,
-        //     IpAddress = "0.0.0.0"
-        // });
         var connectionStrings = await cluster.ConnectionStrings.GetValue();
         var connectionString = connectionStrings[0].StandardSrv ?? string.Empty;
         return new(cluster, connectionString, databasePassword);
