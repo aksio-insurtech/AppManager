5482c9e31ae1a5443101cf9ebe460043b8575eb2
Einar Ingebrigtsen <einari@me.com>
2022-10-18 15:51:52 +0200

    Fixing navigation header. Looking a lot better

--- Source/Web/Header.tsx
+++ Source/Web/Header.tsx
@@ -1,24 +1,128 @@
 // Copyright (c) Aksio Insurtech. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-import { AppBar, Box, Button, Link, Menu, MenuItem, Typography } from '@mui/material';
-import { Stack } from '@mui/system';
+import { AppBar, Box, Button, Container, IconButton, Menu, MenuItem, Toolbar, Typography } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
+import AdbIcon from '@mui/icons-material/Adb';
+import MenuIcon from '@mui/icons-material/Menu';
+import React from 'react';
+
+type page = {
+    name: string;
+    route: string
+}
+
+const pages: page[] = [
+    { name: 'Home', route: '/' },
+    { name: 'Applications', route: '/applications' },
+    { name: 'Organization', route: '/organization' }];
 
 export const Header = () => {
     const navigate = useNavigate();
 
+    const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
+    const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
+
+    const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorElNav(event.currentTarget);
+    };
+
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+    };
+
+    const navigateToPage = (page) => {
+        handleCloseNavMenu();
+        navigate(page.route);
+    };
+
     return (
-        <div>
-            <Box sx={{ flexGrow: 1 }}>
-                <AppBar position="static">
-                    <Stack spacing={2} direction="row">
-                        <Button variant="contained" onClick={() => navigate('/')} >Home</Button>
-                        <Button variant="contained" onClick={() => navigate('/applications')}> Applications</Button>
-                        <Button variant="contained" onClick={() => navigate('/organization')}>Organization settings</Button>
-                    </Stack>
-                </AppBar>
-            </Box>
-        </div >
+        <AppBar position="static" color="primary">
+            <Container maxWidth="xl">
+                <Toolbar disableGutters>
+                    <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+                    <Typography
+                        variant="h6"
+                        noWrap
+                        component="a"
+                        href="/"
+                        sx={{
+                            mr: 2,
+                            display: { xs: 'none', md: 'flex' },
+                            fontFamily: 'monospace',
+                            fontWeight: 700,
+                            letterSpacing: '.3rem',
+                            color: 'inherit',
+                            textDecoration: 'none',
+                        }}>
+                        AppManager
+                    </Typography>
+
+                    <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+                        <IconButton
+                            size="large"
+                            aria-label="account of current user"
+                            aria-controls="menu-appbar"
+                            aria-haspopup="true"
+                            onClick={handleOpenNavMenu}
+                            color="inherit">
+                            <MenuIcon />
+                        </IconButton>
+                        <Menu
+                            id="menu-appbar"
+                            anchorEl={anchorElNav}
+                            anchorOrigin={{
+                                vertical: 'bottom',
+                                horizontal: 'left',
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'left',
+                            }}
+                            open={Boolean(anchorElNav)}
+                            onClose={handleCloseNavMenu}
+                            sx={{
+                                display: { xs: 'block', md: 'none' },
+                            }}>
+                            {pages.map((page) => (
+                                <MenuItem key={page.name} onClick={() => navigateToPage(page)}>
+                                    <Typography textAlign="center">{page.name}</Typography>
+                                </MenuItem>
+                            ))}
+                        </Menu>
+                    </Box>
+
+                    <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+                    <Typography
+                        variant="h5"
+                        noWrap
+                        component="a"
+                        href=""
+                        sx={{
+                            mr: 2,
+                            display: { xs: 'flex', md: 'none' },
+                            flexGrow: 1,
+                            fontFamily: 'monospace',
+                            fontWeight: 700,
+                            letterSpacing: '.3rem',
+                            color: 'inherit',
+                            textDecoration: 'none',
+                        }}>
+                        AppManager
+                    </Typography>
+                    <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+                        {pages.map((page) => (
+                            <Button
+                                key={page.name}
+                                onClick={() => navigateToPage(page)}
+                                sx={{ my: 2, color: 'white', display: 'block' }}>
+                                {page.name}
+                            </Button>
+                        ))}
+                    </Box>
+                </Toolbar>
+            </Container>
+        </AppBar >
     );
 };
