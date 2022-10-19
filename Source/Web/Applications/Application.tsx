2d7911c4c970fbbbd0f949d87a377a05174e8745
Einar Ingebrigtsen <einari@me.com>
2022-10-19 17:25:12 +0200

    Started looking at tabs

--- Source/Web/Applications/Application.tsx
+++ Source/Web/Applications/Application.tsx
@@ -5,13 +5,33 @@ import { General } from './General';
 import { Tenants } from './Tenants';
 import { GetApplication } from 'API/applications/GetApplication';
 import { useParams } from 'react-router-dom';
+import { Box, Tab } from '@mui/material';
+import { TabContext, TabList, TabPanel } from '@mui/lab';
+import { useState } from 'react';
 
 export const Application = () => {
     const { applicationId } = useParams();
     const [application] = GetApplication.use({ applicationId: applicationId! });
+    const [selectedTab, setSelectedTab] = useState("0");
 
     return (
-        <>Katt</>
+        <>
+            <TabContext value={selectedTab}>
+
+                <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                    <TabList onChange={(e, value) => setSelectedTab(value)}>
+                        <Tab label="First" value="0" />
+                        <Tab label="Second" value="1" />
+                        <Tab label="Third" value="2" />
+                    </TabList>
+                </Box>
+                <TabPanel value="0">First</TabPanel>
+                <TabPanel value="1">Second</TabPanel>
+                <TabPanel value="2">Third</TabPanel>
+
+            </TabContext>
+
+        </>
         // <Stack style={{ height: '100%' }}>
         //     <Stack.Item>
         //         <Pivot>
