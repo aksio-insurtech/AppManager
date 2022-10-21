061ba05405ab6558ad241e12087a2cf0f1d44114
Einar Ingebrigtsen <einari@me.com>
2022-10-21 16:07:58 +0200

    Making filters work with property path instead

--- Source/Read/Filters.cs
+++ Source/Read/Filters.cs
@@ -3,7 +3,6 @@
 
 using System.Linq.Expressions;
 using Aksio.Cratis.Reflection;
-using Aksio.Cratis.Strings;
 
 namespace Read;
 
@@ -12,8 +11,8 @@ public static class Filters
     public static FilterDefinition<T> StringFilterFor<T>(Expression<Func<T, object>> property, object value)
         where T : notnull
     {
-        var propertyInfo = property.GetPropertyInfo();
-        var name = propertyInfo.Name.ToCamelCase();
-        return Builders<T>.Filter.Eq(new StringFieldDefinition<T, string>(name), value.ToString()!);
+        var propertyPath = property.GetPropertyPath();
+        var actualPropertyPath = string.Join('.', propertyPath.Segments.Select(_ => _.Value.Equals("id") ? "_id" : _.Value));
+        return Builders<T>.Filter.Eq(new StringFieldDefinition<T, string>(actualPropertyPath), value.ToString()!);
     }
 }
