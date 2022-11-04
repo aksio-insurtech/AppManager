5dba131be113da053ed83787084d3e176d6b778a
Einar Ingebrigtsen <einari@me.com>
2022-11-04 12:24:26 +0100

    UPgrade Cratis

--- Docker/Kernel/dockerize.sh
+++ Docker/Kernel/dockerize.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker buildx build -t opensjoncontainerapp4716a5a2.azurecr.io/cratis:6.15.9 -f ./Dockerfile --platform linux/amd64 --push ../
+docker buildx build -t opensjoncontainerapp4716a5a2.azurecr.io/cratis:6.15.10 -f ./Dockerfile --platform linux/amd64 --push ../
