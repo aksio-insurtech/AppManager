c518a34fb223c4a17cba8642a9ee83802ae80dc2
Einar Ingebrigtsen <einari@me.com>
2022-11-17 15:51:17 +0100

    Fixing ordering of usings

--- Source/Bootstrap/ApplicationAndEnvironment.cs
+++ Source/Bootstrap/ApplicationAndEnvironment.cs
@@ -1,8 +1,8 @@
 // Copyright (c) Aksio Insurtech. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using Reactions.Applications;
 using Concepts.Applications;
+using Reactions.Applications;
 
 namespace Bootstrap;
 
