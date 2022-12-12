549669687c58831237066c34d59847fde1f5e676
Einar Ingebrigtsen <einari@me.com>
2022-12-12 12:23:56 +0100

    Commenting out connection string fetching

--- Source/Reactions/Applications/Pulumi/Resources/ServiceBusResourceRenderer.cs
+++ Source/Reactions/Applications/Pulumi/Resources/ServiceBusResourceRenderer.cs
@@ -37,16 +37,16 @@ public class ServiceBusResourceRenderer : ICanRenderResource<ServiceBusConfigura
             }
         });
 
-        var namespaceKeys = Output
-            .Tuple(@namespace.Name, namespaceRule.Name)
-            .Apply(_ => ListNamespaceKeys.InvokeAsync(new()
-            {
-                NamespaceName = _.Item1,
-                AuthorizationRuleName = _.Item2,
-                ResourceGroupName = context.ResourceGroup.GetResourceName()
-            }));
-
-        var namespaceKeysValue = await namespaceKeys.GetValue();
-        Console.WriteLine(namespaceKeysValue.PrimaryConnectionString);
+        // TODO: Register connection string result
+        // var namespaceKeys = Output
+        //     .Tuple(@namespace.Name, namespaceRule.Name)
+        //     .Apply(_ => ListNamespaceKeys.InvokeAsync(new()
+        //     {
+        //         NamespaceName = _.Item1,
+        //         AuthorizationRuleName = _.Item2,
+        //         ResourceGroupName = context.ResourceGroup.GetResourceName()
+        //     }));
+        // var namespaceKeysValue = await namespaceKeys.GetValue();
+        // Console.WriteLine(namespaceKeysValue.PrimaryConnectionString);
     }
 }
