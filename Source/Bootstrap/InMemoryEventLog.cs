31ff00ee53d66b18778b821a751b3bdea10914c6
Einar Ingebrigtsen <einari@me.com>
2023-03-22 12:20:38 +0100

    Adding default not implemented implementations

--- Source/Bootstrap/InMemoryEventLog.cs
+++ Source/Bootstrap/InMemoryEventLog.cs
@@ -6,4 +6,9 @@ namespace Bootstrap;
 public class InMemoryEventLog : IEventLog
 {
     public Task Append(EventSourceId eventSourceId, object @event, DateTimeOffset? validFrom = null) => Task.CompletedTask;
+    public Task<EventSequenceNumber> GetNextSequenceNumber() => throw new NotImplementedException();
+    public Task<EventSequenceNumber> GetTailSequenceNumber() => throw new NotImplementedException();
+    public Task<EventSequenceNumber> GetTailSequenceNumberForObserver(Type type) => throw new NotImplementedException();
+    public Task Redact(EventSequenceNumber sequenceNumber, RedactionReason? reason = null) => throw new NotImplementedException();
+    public Task Redact(EventSourceId eventSourceId, RedactionReason? reason = null, params Type[] eventTypes) => throw new NotImplementedException();
 }
