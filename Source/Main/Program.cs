456bbfb0f498bc347243c6f6e3ec30e963570e15
Einar Ingebrigtsen <einari@me.com>
2023-07-26 10:49:01 +0200

    Making it multi-tenanted

--- Source/Main/Program.cs
+++ Source/Main/Program.cs
@@ -6,7 +6,9 @@ using Sample;
 var builder = Host.CreateDefaultBuilder()
                     .UseMongoDB()
                     .UseAksio()
-                    .UseCratis(_ => _.ForMicroservice("8c538618-2862-4018-b29d-17a4ec131958", "Management"))
+                    .UseCratis(_ => _
+                        .ForMicroservice("8c538618-2862-4018-b29d-17a4ec131958", "Management")
+                        .MultiTenanted())
                     .ConfigureWebHostDefaults(_ => _.UseStartup<Startup>());
 
 var app = builder.Build();
